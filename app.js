7127a384c06a2ccc2ce8bfefcc0763eb5755bd23
Lisonallie <allisonvanlinden@gmail.com>
2019-07-22 12:01:48 +0200

    add 2 canvases for graphs

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let body = document.getElementById("bodyContent");
 let canvas1 = document.createElement("canvas");
+let table1 = document.querySelector("#table1 > caption");
+table1.insertBefore(canvas1, table1.childNodes[0]);
+
+console.log(canvas1);
+
+let canvas2 = document.createElement("canvas");
+let table2 = document.querySelector("#table2 > caption");
+table2.insertBefore(canvas2, table2.childNodes[0]);
+
+console.log(canvas2);
