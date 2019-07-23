7eb48da76219a104e19abb244f2ad5f593fc1192
Lisonallie <allisonvanlinden@gmail.com>
2019-07-23 09:30:25 +0200

    add id to canvas elements

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 let canvas1 = document.createElement("canvas");
+canvas1.id = "firstGraph";
 let table1 = document.querySelector("#table1 > caption");
 table1.insertBefore(canvas1, table1.childNodes[0]);
 
 console.log(canvas1);
 
 let canvas2 = document.createElement("canvas");
+canvas2.id = "secondGraph";
 let table2 = document.querySelector("#table2 > caption");
 table2.insertBefore(canvas2, table2.childNodes[0]);
 
 console.log(canvas2);
 
 let canvas3 = document.createElement("canvas");
+canvas3.id = "thirdGraph";
 let body = document.getElementById("bodyContent");
 body.insertBefore(canvas3, body.childNodes[0]);
 
-console.log(canvas3)
+console.log(canvas3);
+
+
