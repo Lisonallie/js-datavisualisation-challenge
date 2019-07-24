34407f7f06c41f2d488eeec192a7fc601495c8da
Lisonallie <allisonvanlinden@gmail.com>
2019-07-24 14:23:19 +0200

    go to previous commit

--- app.js
+++ app.js
@@ -27,36 +27,39 @@ caption2.insertBefore(canvas3, caption2.childNodes[0]);
 
 
 
+async function getData() {
+    const request = "http://127.0.0.1:5500/test.json";
+    let response = await fetch(request);
+    graphData = await response.json();
 
-
-let xData = [];
-let yData = [];
-let dataSets3 = [];
-for (let i = 2; i < contents.length; i += 6) {
-    xData = contents[i];
-    let json = {
-        label: ['Cuurent Statistics'], //countries
-        data: yData, //numbers
-        backgroundColor: RBG(),
+    let xData = [];
+    let yData = [];
+    let dataSets3 = [];
+    for (let i = 2; i < contents.length; i += 6) {
+        xData = contents[i];
+        let json = {
+            label: ['Cuurent Statistics'], //countries
+            data: yData, //numbers
+            backgroundColor: RBG(),
+        }
+        dataSets3.push(json);
     }
-    dataSets3.push(json);
-}
 
-for (let j = 4; j < contents.length; j += 6) {
-    yData = contents[j];
-}
+    for (let j = 4; j < contents.length; j += 6) {
+        yData = contents[j];
+    }
 
-let ctx3 = document.getElementById("firstGraph").getContext('2d');
-let myChart3 = new Chart(ctx3, {
-    type: 'line',
-    data: {
-        labels: xData,
-        datasets: dataSets3,
-    },
-    // Configuration options go here
-    options: {}
-});
-console.log(myChart3);
+    let ctx3 = document.getElementById("firstGraph").getContext('2d');
+    let myChart3 = new Chart(ctx3, {
+        type: 'line',
+        data: {
+            labels: xData,
+            datasets: dataSets3,
+        },
+        // Configuration options go here
+        options: {}
+    });
+}
 
 
 
@@ -74,7 +77,7 @@ let dataSets = [];
 //----------------YEAR---------------
 for (let i = 2; i < table1.rows[1].cells.length; i++) {
     yearData[i - 2] = table1.rows[1].cells[i].innerHTML;
-
+    
 }
 
 function RBG() {
@@ -99,16 +102,16 @@ for (let j = 2; j < table1.rows.length; j++) {
     table1.rows[29].cells[1].innerHTML = "Islande";
     table1.rows[32].cells[1].innerHTML = "Suisse";
     table1.rows[36].cells[1].innerHTML = "Turquie";
-
-
+    
+    
     let numbersData = []; //-------array has to be here or it runs 36 times--------
     let tableRow = table1.rows[j];
     let jsonData = {
         label: countryData, //countries
         data: numbersData, //numbers
-        backgroundColor: RBG(),
+        backgroundColor: RBG(),     
     };
-
+    
     //--------------------NUMBERS----------------
     for (let k = 2; k < tableRow.cells.length; k++) {
         numbersData.push(parseInt(tableRow.cells[k].innerHTML));
@@ -142,7 +145,7 @@ let dataSets2 = [];
 //----------------YEAR---------------
 for (let i = 2; i < table2.rows[1].cells.length; i++) {
     yearData2[i - 2] = table2.rows[0].cells[i].innerHTML;
-
+    
 }
 
 function callRBG() {
@@ -152,25 +155,25 @@ function callRBG() {
     let b = num & 255;
     return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
-
+ 
 
 
 //---------------COUNTRY--------------------
-for (let j = 1; j < table2.rows.length; j++) {
+for (let j = 2; j < table2.rows.length; j++) {
     countryData2 = table2.rows[j].cells[1].innerHTML;
     table2.rows[8].cells[1].innerHTML = "paysdeGalles(UK)"
     table2.rows[9].cells[1].innerHTML = "Ecosse(UK)"
-
+    
     let numbersData2 = []; //-------array has to be here or it runs 36 times--------
     let tableRow2 = table2.rows[j];
     let jsonData2 = {
         label: countryData2, //countries
         data: numbersData2, //numbers
-        backgroundColor: callRBG(),
+        backgroundColor: callRBG(),     
     };
     // console.log(countryData2);
-
-
+    
+    
     //--------------------NUMBERS----------------
     for (let k = 2; k < tableRow2.cells.length; k++) {
         numbersData2.push(parseInt(tableRow2.cells[k].innerHTML));
@@ -188,3 +191,5 @@ let myChart2 = new Chart(ctx2, {
     options: {
     }
 });
+
+console.log(dataSets2);
